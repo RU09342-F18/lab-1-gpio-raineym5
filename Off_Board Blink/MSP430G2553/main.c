10b7d32df6d0a7a178778a1f2396e47b2ef82d8b
raineym5 <raineym5@students.rowan.edu>
2018-09-18 08:59:56 -0400

    MSP430G2553 off-board blink code

---
+++ Off_Board Blink/MSP430G2553/main.c
@@ -0,0 +1,21 @@
+#include <msp430.h> 
+
+
+/**
+ * main.c
+ */
+//volatile int8 receivedchar;
+void main(void){
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	P1DIR = 0xff;
+	P2DIR = 0xff;
+	P3DIR = 0xff;
+	P1OUT = 0x00;
+
+	while(1){
+	    P1OUT ^= BIT3;
+	    __delay_cycles(1000000);
+
+	}
+
+}
